6729b1d159ea9e0a75c3b884426acd657bdd5365
ujjawal0619 <ujjawal.kumar0619@gmail.com>
2024-11-17 22:08:08 +0530

    account repo and service done

--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -1,5 +1,20 @@
-CreateAccount
+package main
 
-CreateProduct
+import "context"
 
-CreateOrder
+type mutationResolver struct {
+	server *Server
+}
+
+func (r *mutationResolver) createAccount(ctx context.Context, in AccountInput) (*Account, error) {
+	panic("unimplemented")
+}
+
+func (r *mutationResolver) createProduct(ctx context.Context, in ProductInput) (*Product, error) {
+	panic("unimplemented")
+
+}
+
+func (r *mutationResolver) createOrder(ctx context.Context, in OrderInput) (*Order, error) {
+	panic("unimplemented")
+}
